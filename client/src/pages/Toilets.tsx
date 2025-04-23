79dc99996ed64975e386518b7c5108b984cdc8ec
MBoussel <mb.boussel@gmail.com>
2025-04-23 20:32:05 +0200

    feat: change css body

--- client/src/pages/Toilets.tsx
+++ client/src/pages/Toilets.tsx
@@ -3,9 +3,9 @@ import CardListToilets from "../components/CardListToilets";
 import CardTitle from "../components/CardTitle";
 export default function Toilets() {
   return (
-    <>
+    <div className="toiletsPage">
       <CardTitle />
       <CardListToilets />
-    </>
+    </div>
   );
 }
