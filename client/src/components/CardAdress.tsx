07f6aa002c29a2c24d3c2542616c07eca385dfca
MBoussel <mb.boussel@gmail.com>
2025-04-23 20:29:12 +0200

    feat: import geolocation img

--- client/src/components/CardAdress.tsx
+++ client/src/components/CardAdress.tsx
@@ -1,12 +1,9 @@
+import Geolocation from "../assets/images/geolocation.png";
 export default function CardAdress() {
   return (
     <>
       <div className="cardAdressInfo">
-        <img
-          className="geolocation"
-          src="./src/assets/images/geolocation.png"
-          alt="ping"
-        />
+        <img className="geolocation" src={Geolocation} alt="ping" />
         <span className="rue">125 Avenue LEDRU ROLLIN</span>
         <button className="view" type="button">
           Voir sur la carte
