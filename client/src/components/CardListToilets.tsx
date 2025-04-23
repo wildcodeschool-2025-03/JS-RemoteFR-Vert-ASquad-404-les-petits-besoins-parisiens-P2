9fb00a8532a4f6afa90879387a8eeb256b54d24f
MBoussel <mb.boussel@gmail.com>
2025-04-23 17:34:26 +0200

    feat: import ToiletsLogo

--- client/src/components/CardListToilets.tsx
+++ client/src/components/CardListToilets.tsx
@@ -1,14 +1,11 @@
 import CardAdress from "./CardAdress";
+import ToiletsLogo from "./src/assets/images/map-Paris.png";
 
 export default function CardListToilets() {
   return (
     <article className="cardList">
       <div className="cardMap">
-        <img
-          className="map"
-          src="./src/assets/images/map-Paris.png"
-          alt="mapParis"
-        />
+        <img className="map" src={ToiletsLogo} alt="mapParis" />
       </div>
       <CardAdress />
       <CardAdress />
