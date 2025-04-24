ba64c880361c9eb16030f09cafa483eba1e9c7f9
K-Smagghe <kevin.smagghe.pro@gmail.com>
2025-04-24 14:10:14 +0200

    feat: add navbar

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -2,26 +2,30 @@ import "../assets/styles/homepage.css";
 import mascotte from "../assets/images/Mascotte.svg";
 import homeIcone from "../assets/images/home.png";
 import background from "../assets/images/train.jpg";
+import NavBar from "../components/Navbar";
 
 function HomePage() {
   return (
     <>
-      <div className="container3">
-        <div className="container2">
-          <div className="container1">
-            <a href="/">
-              <img src={homeIcone} alt="Icone Home" className="home" />
-            </a>
-            <h1>
-              Les petits
-              <br />
-              besoins
-              <br />
-              parisiens
-            </h1>
-            <img src={mascotte} alt="mascotte" className="mascotte" />
+      <div className="container4">
+        <div className="container3">
+          <div className="container2">
+            <div className="container1">
+              <a href="/">
+                <img src={homeIcone} alt="Icone Home" className="home" />
+              </a>
+              <h1>
+                Les petits
+                <br />
+                besoins
+                <br />
+                parisiens
+              </h1>
+              <img src={mascotte} alt="mascotte" className="mascotte" />
+            </div>
+            <p className="paragraphe">Trouver à proximité</p>
           </div>
-          <p className="paragraphe">Trouver à proximité</p>
+          <NavBar />
         </div>
         <img src={background} alt="train" className="picturetrain" />
       </div>
