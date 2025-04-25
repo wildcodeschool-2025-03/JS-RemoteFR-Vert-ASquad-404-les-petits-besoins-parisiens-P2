2b3168e363039c2f52cd5ad5a14aa1b19344f0d2
SPerrierPro <sophie.perrier.pro@gmail.com>
2025-04-25 14:37:56 +0200

    feat: added style and general display to the store page

--- client/src/pages/StoresPage.tsx
+++ client/src/pages/StoresPage.tsx
@@ -1,7 +1,20 @@
-import CardList from "../components/CardList";
+import "../assets/styles/card.css";
+import CardTitle from "../components/CardTitle";
+import NavBar from "../components/Navbar";
+import StoreCardList from "../components/StoreCardList";
+import ToiletsLogo from "../assets/images/map-Paris.png";
 
 function StoresPage() {
-  return <CardList />;
+  return (
+    <article className="cardList">
+      <CardTitle />
+      <div className="cardMap">
+        <img className="map" src={ToiletsLogo} alt="mapParis" />
+      </div>
+      <StoreCardList />
+      <NavBar />
+    </article>
+  );
 }
 
 export default StoresPage;
