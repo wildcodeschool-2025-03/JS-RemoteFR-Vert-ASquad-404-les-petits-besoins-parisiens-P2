4b7109af7cf7da40e6860a2aa97c78bf24577889
K-Smagghe <kevin.smagghe.pro@gmail.com>
2025-04-30 09:58:26 +0200

    style: update style in the defibrillator page

--- client/src/components/DefibrillatorCard.tsx
+++ client/src/components/DefibrillatorCard.tsx
@@ -1,3 +1,4 @@
+import "../assets/styles/defibrillatorcard.css";
 import Geolocation from "../assets/images/geolocation.png";
 type defibrillatorsType = {
   lat_coor1: string;
@@ -18,13 +19,56 @@ type defibrillatorsType = {
 
 type Props = { defibrillator: defibrillatorsType };
 export default function DefibrillatorCard(_props: Props) {
+  /* function pour les arrondissement*/
+  function arrondissement(arrondissement: number) {
+    if (arrondissement === 1) {
+      return "1er";
+    }
+    if (arrondissement === 2) {
+      return "2nd";
+    }
+    return ` ${arrondissement}ème `;
+  }
+
+  /* Function pour l'adresse Cité incomplete */
+  function cite(cite: string) {
+    if (cite.includes("Cité")) {
+      return `Métro ${_props.defibrillator.adr_voie}`;
+    }
+    return _props.defibrillator.adr_voie;
+  }
+
   return (
-    <div className="cardAdressInfo">
-      <img className="geolocation" src={Geolocation} alt="ping" />
-      <span className="rue">125 Avenue du defibrillateurs</span>
-      <button className="view" type="button">
-        Voir sur la carte
-      </button>
+    <div className="cardAdressdefibrillators">
+      <div className="firstline">
+        <img className="geolocation" src={Geolocation} alt="ping" />
+        <span className="rue">{cite(_props.defibrillator.adr_voie)}</span>
+      </div>
+      <div className="horaires">
+        <article className="oneline arrondissement">
+          <h3>
+            {" "}
+            {arrondissement(_props.defibrillator.com_cp % 100)} arrondissement
+          </h3>
+        </article>
+        <article className="oneline">
+          <h3>Horaires : </h3>
+          {_props.defibrillator.dispo_h}
+        </article>
+      </div>
+      <div className="pmr">
+        <article className="oneline">
+          <h3>Disponibilité: </h3> {_props.defibrillator.dispo_i}
+        </article>
+        <article className="oneline">
+          <h3>Accès libre :</h3> {_props.defibrillator.acc_lib}
+        </article>
+      </div>
+      <article className="buttonView">
+        <button className="view" type="button">
+          Voir sur la carte
+        </button>
+      </article>
     </div>
   );
 }
