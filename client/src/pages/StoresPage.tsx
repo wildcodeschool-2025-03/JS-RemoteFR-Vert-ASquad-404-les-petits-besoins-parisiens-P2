fed36a3c3c22f9cba62d4a08438096e6fa861b87
marina saraiva <marinasaraiva@hotmail.fr>
2025-04-30 16:58:01 +0200

    feat: integration of the navigation bar and home button in all pages. improvement CSS

--- client/src/pages/StoresPage.tsx
+++ client/src/pages/StoresPage.tsx
@@ -1,7 +1,19 @@
 import CardList from "../components/CardList";
+import NavBarDesktop from "../components/NavBarDesktop";
+import NavBar from "../components/Navbar";
 
 function StoresPage() {
-  return <CardList />;
+  return (
+    <div>
+      <div className="navBarDesktop">
+        <NavBarDesktop />
+      </div>
+      <CardList />
+      <div className="nav-mobile desktop">
+        <NavBar />
+      </div>
+    </div>
+  );
 }
 
 export default StoresPage;
