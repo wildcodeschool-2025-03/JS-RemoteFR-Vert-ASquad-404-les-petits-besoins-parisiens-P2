0f222a472c37daabf56cb56b7c0d237f17511946
K-Smagghe <kevin.smagghe.pro@gmail.com>
2025-05-13 11:33:16 +0200

    feat: add buttonlike

---
+++ client/src/components/ButtonLike.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "../assets/styles/ButtonLike.css"
+
+function ButtonLike () {
+   const [like,setlike] = useState(false);
+
+   const handlelike = () => {
+    setlike(!like);
+   }
+
+    return <button className="buttonLike" type="button" onClick={handlelike}>
+        {like ? "❤️" : "🤍"}
+    </button>
+}
+
+export default ButtonLike
