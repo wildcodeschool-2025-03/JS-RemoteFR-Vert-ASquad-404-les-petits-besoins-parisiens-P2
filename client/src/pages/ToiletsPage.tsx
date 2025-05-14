73acfe69967a699e6ae1f9efefa4b6c0919a60bc
K-Smagghe <kevin.smagghe.pro@gmail.com>
2025-05-14 17:34:12 +0200

    feat: filters

--- client/src/pages/ToiletsPage.tsx
+++ client/src/pages/ToiletsPage.tsx
@@ -1,4 +1,5 @@
 import "../assets/styles/card.css";
+import { useState } from "react";
 import mapParis from "../assets/images/map-Paris.png";
 import CardListToilets from "../components/CardListToilets";
 import CardTitle from "../components/CardTitle";
@@ -7,6 +8,10 @@ import NavBarDesktop from "../components/NavBarDesktop";
 import NavBar from "../components/Navbar";
 
 export default function ToiletsPage() {
+  const [codePostalFilter, setCodePostalFilter] = useState("");
+  const [accesPmrFilter, setAccesPmrFilter] = useState(false);
+  const [relaisBebeFilter, setRelaisBebeFilter] = useState(false);
+
   return (
     <div className="page">
       <div className="dark-mode">
@@ -23,7 +28,36 @@ export default function ToiletsPage() {
         <div className="nav-mobile desktop">
           <NavBar />
         </div>
-        <CardListToilets />
+
+        <div className="filters">
+          <input
+            type="text"
+            id="codePostal"
+            placeholder="Filtrer par code postal"
+            value={codePostalFilter}
+            onChange={(e) => setCodePostalFilter(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="checkbox"
+            id="accesPmr"
+            checked={accesPmrFilter}
+            onChange={(e) => setAccesPmrFilter(e.target.checked)}
+          />
+          <p> Acces PMR</p>
+        </div>
+        <div>
+          <input
+            type="checkbox"
+            id="relaisBebe"
+            checked={relaisBebeFilter}
+            onChange={(e) => setRelaisBebeFilter(e.target.checked)}
+          />
+          <p>Relais bébé</p>
+        </div>
+
+        <CardListToilets/>
       </article>
     </div>
   );
