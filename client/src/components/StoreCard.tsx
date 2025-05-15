425cedcc352183fbe408d3d44a7055844a0299c5
MBoussel <mb.boussel@gmail.com>
2025-05-15 15:27:58 +0200

    feat: create map for store page

--- client/src/components/StoreCard.tsx
+++ client/src/components/StoreCard.tsx
@@ -1,7 +1,5 @@
 import Geolocation from "../assets/images/geolocation.png";
 import "../assets/styles/shopCard.css";
-import { useTranslation } from "../contexts/LocaleContext";
-import ButtonLike from "./ButtonLike";
 
 type storeType = {
   tco_libelle: string;
@@ -10,47 +8,51 @@ type storeType = {
   dea_jour_fermeture: string;
   dea_rue_livraison: string;
   dea_cp_livraison: number;
+  dea_commune_livraison: string;
   geocodage_ban: {
     lon: number;
     lat: number;
   };
 };
+type Poi = { key: string; location: google.maps.LatLngLiteral; color: string };
+type Props = {
+  store: storeType;
+  setPoiCenter: React.Dispatch<React.SetStateAction<Poi>>;
+};
 
-type Props = { store: storeType };
-
-function StoreCard({ store }: Props) {
-  /* function pour les arrondissement*/
-  function eniem(eniem: number) {
-    if (eniem === 1) {
-      return "1er";
-    }
-    if (eniem === 2) {
-      return "2nd";
-    }
-    return ` ${eniem}ème `;
-  }
-  const { translations } = useTranslation();
+function StoreCard({ store, setPoiCenter }: Props) {
   return (
     <div className="cardAdressInfoShop">
       <div className="shop-header">
         <img className="geolocation-shop" src={Geolocation} alt="ping" />
-        <span className="shop-adress">{store.dea_rue_livraison}</span>
+        <span className="shop-adress">
+          {store.dea_rue_livraison}, {store.dea_cp_livraison}{" "}
+          {store.dea_commune_livraison}
+        </span>
       </div>
       <span className="shop-info">
-        <p>{eniem(store.dea_cp_livraison % 100)} arrondissement</p>
-        <div className="separation" />
         <p>
           {store.tco_libelle} | {store.dea_nom_commerce}
         </p>
-        <p className="code-postal">
-          {translations.shopCard.closedOn}
-          {store.dea_jour_fermeture}
-        </p>
+        <p className="code-postal">Fermé le : {store.dea_jour_fermeture}</p>
       </span>
-      <button className="button-shop" type="button">
-        {translations.shopCard.view}
+      <button
+        className="button-shop"
+        type="button"
+        onClick={() => {
+          window.scrollTo({ top: 200, behavior: "smooth" });
+          setPoiCenter({
+            key: store.dea_nom_commerce,
+            location: {
+              lat: store.geocodage_ban.lat,
+              lng: store.geocodage_ban.lon,
+            },
+            color: "#A14AFF",
+          });
+        }}
+      >
+        Voir sur la carte
       </button>
-      <ButtonLike />
     </div>
   );
 }
--- client/src/pages/StoresPage.tsx
+++ client/src/pages/StoresPage.tsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import "../assets/styles/card.css";
-import mapParis from "../assets/images/map-Paris.png";
 import CardListShop from "../components/CardListShop";
 import CardTitle from "../components/CardTitle";
 import DarkModeButton from "../components/DarkModeButton";
+import MyMaps from "../components/MyMaps/MyMaps";
 import NavBarDesktop from "../components/NavBarDesktop";
 import NavBar from "../components/Navbar";
 import ScrollToTopButton from "../components/ScrollButton";
 
 function StoresPage() {
+  type Poi = {
+    key: string;
+    location: google.maps.LatLngLiteral;
+    color: string;
+  };
+  const centerParis: Poi = {
+    key: "Paris",
+    location: { lat: 48.8583276, lng: 2.2944867 },
+    color: "#A14AFF",
+  };
+  const [poiCenter, setPoiCenter] = useState(centerParis);
   return (
     <div className="page">
       <div className="dark-mode">
@@ -18,13 +30,13 @@ function StoresPage() {
       </div>
       <article className="cardList">
         <CardTitle />
-        <div className="cardmapStyle">
-          <img className="mapStyle" src={mapParis} alt="mapParis" />
+        <div>
+          <MyMaps poiCenter={poiCenter} />
         </div>
         <div className="nav-mobile desktop">
           <NavBar />
         </div>
-        <CardListShop />
+        <CardListShop setPoiCenter={setPoiCenter} />
       </article>
       <ScrollToTopButton />
     </div>
